2b0727f83c2d53beca628227b444e6449874df70
Yoss!E <4yossiedri@gmail.com>
2015-12-14 21:49:21 +0200

    fix signup and welcome email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,8 @@ class Mailer < ApplicationMailer
 	default from: "info@roooster.net"
 
 	def welcome_email(user)
-		mail(to: user.email,subject: "welcome to roooster!")
+		@user = user
+		mail(to: @user.email,subject: "Welcome to Roooster!")
 	end
 
 	def invite_email(inviter,invitee)
