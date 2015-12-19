6f72dae3eb3208e0e16d07caf98bbdaff3b7be2c
Yoss!E <4yossiedri@gmail.com>
2015-12-19 23:08:19 +0200

    add user list show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   after_action :verify_authorized
 
+  layout 'admin'
+
   def index
     @users = User.all
     authorize User
   end
-
+  
   def show
     @user = User.find(params[:id])
     authorize @user
