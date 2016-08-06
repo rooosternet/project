80a1655080d0f4b050b66e14ec772eea1437f2a0
YossiEdri <4yossiedri@gmail.com>
2016-08-06 10:38:13 +0300

    reset password fix

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -25,6 +25,35 @@ class Users::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
+   # GET /resource/password/edit?reset_password_token=abcdef
+  def edit
+    self.resource = resource_class.new
+    set_minimum_password_length
+    resource.reset_password_token = params[:reset_password_token]
+    @reset_password = true
+    render 'visitors/home' 
+  end
+
+  # PUT /resource/password
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_flashing_format?
+        sign_in(resource_name, resource)
+      else
+        set_flash_message(:notice, :updated_not_active) if is_flashing_format?
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
   # protected
   protected
 
