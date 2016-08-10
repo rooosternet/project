27f1f47a2d239e115a61352a690fa56cc538de03
Trishin Dmitry <Smartkrio@MacBook-Pro-Dmitrii.local>
2016-08-10 07:22:08 +0300

    new mail settings to staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.email_server,
-    port: 587,
+    port: 465,
+    ssl: true,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
