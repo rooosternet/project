4c57e8b54cbadb1ae5bac9366fae8bdd04df705e
Trishin Dmitry <Smartkrio@MacBook-Pro-Dmitrii.local>
2016-08-30 08:36:21 +0300

    change order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@
 // require javascripts/bootstrap-sprockets
 // require avatars_for_rails
 //= require jquery.autosize
-//= require websocket_rails/main
 //= require_tree .
+//= require websocket_rails/main
 
 
