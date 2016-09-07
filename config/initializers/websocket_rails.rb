29d66db1d0138acff94280df645dc6459de1f653
Trishin Dmitry <Smartkrio@MacBook-Pro-Dmitrii.local>
2016-09-08 02:32:18 +0300

    set redis timeout 0

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -16,7 +16,7 @@ WebsocketRails.setup do |config|
   # Start the standalone server with rake websocket_rails:start_server
   # * Requires Redis
   config.standalone = true
-  config.redis_options = {:host => 'localhost', :port => '6379', :timeout => 15 }
+  config.redis_options = {:host => 'localhost', :port => '6379', :timeout => 0 }
 
   # Change to true to enable channel synchronization between
   # multiple server instances.
