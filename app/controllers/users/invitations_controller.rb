3a3f6722770cdc525bb78c4badea6e50be33a0d7
Trishin Dmitry <Smartkrio@MacBook-Pro-Dmitrii.local>
2016-09-13 19:09:17 +0300

    remove invitation functional

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,24 +1,23 @@
 class Users::InvitationsController < Devise::InvitationsController
 
-  def create
-    self.resource = invite_resource
-    resource_invited = resource.errors.empty?
-    yield resource if block_given?
-    byebug
-    if resource_invited
-      if is_flashing_format? && self.resource.invitation_sent_at
-        set_flash_message :notice, :send_instructions, :email => self.resource.email
-      end
-      # respond_with resource, :location => after_invite_path_for(current_inviter)
-      render :json => { :responseText => "Invitation sent to #{@user.firstname}." }.to_json , :status => 200
-    else
-      # respond_with_navigational(resource) { render :new }
-      if resource.errors.full_messages.include?("Email has already been taken")
-        message = "Invitation sent to #{@user.firstname}."
-      end
-      render :json => { :responseText => message }.to_json , :status => 500
-    end
-  end
+  # def create
+  #   self.resource = invite_resource
+  #   resource_invited = resource.errors.empty?
+  #   yield resource if block_given?
+  #   if resource_invited
+  #     if is_flashing_format? && self.resource.invitation_sent_at
+  #       set_flash_message :notice, :send_instructions, :email => self.resource.email
+  #     end
+  #     # respond_with resource, :location => after_invite_path_for(current_inviter)
+  #     render :json => { :responseText => "Invitation sent to #{@user.firstname}." }.to_json , :status => 200
+  #   else
+  #     # respond_with_navigational(resource) { render :new }
+  #     if resource.errors.full_messages.include?("Email has already been taken")
+  #       message = "Invitation sent to #{@user.firstname}."
+  #     end
+  #     render :json => { :responseText => message }.to_json , :status => 500
+  #   end
+  # end
 
   # def edit
   #   set_minimum_password_length if respond_to? :set_minimum_password_length
