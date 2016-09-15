d5de523a43b663b4a84f443586b7baa9fc451a77
Trishin Dmitry <Smartkrio@MacBook-Pro-Dmitrii.local>
2016-09-15 12:38:41 +0300

    wip

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,9 +4,11 @@ class InfoController < ApplicationController
 	helper :application
 
 	def about
+		@hide_fullid = true
 	end
 
 	def blog
+		@hide_fullid = true
 		@blog_posts = Post.intros.paginate(page: params[:page], per_page: 3)
 		@normalize_blog_posts = normalize_posts(@blog_posts)
 	end
@@ -17,14 +19,15 @@ class InfoController < ApplicationController
 	# end
 
 	def faq
-		
+		@hide_fullid = true
 	end
 
 	def contact_us
+		@hide_fullid = true
 		unless request.get?
 		  email = Mailer.contact_us(params[:message]).deliver_later if params[:message].is_a?(Hash)
 	      redirect_to root_path
-	    end	
+	    end
 	end
 
 end
